586d2998dc1a87c3637704481678915d2dde4568
roshan-ratheethra <roshratheethra@gmail.com>
2026-02-23 14:02:05 +0530

    Move UC2.java to UC2 branch

---
+++ UC2.java
@@ -0,0 +1,12 @@
+public class UC2 {
+    public static void main(String[] args) {
+        // UC2: Display "OOPS" banner using ASCII art
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *      ");
+        System.out.println("*     * *     * *     * * * * *");
+        System.out.println("*     * *     * * * * *       *");
+        System.out.println("*     * *     * *       *     *");
+        System.out.println(" *****   *****  *        ****** ");
+    }
+}
